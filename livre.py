4fe46a3dd2cad99154316fc38cf172b46a3fde35
Luis Victoria <v@lambda.lv>
2025-05-14 22:16:57 -0400

    Create argument parser

--- livre.py
+++ livre.py
@@ -4,6 +4,7 @@ Expected usage: python livre.py file.lv
 """
 
 import argparse
+import os
 import sys
 
 
@@ -14,11 +15,18 @@ def parse():
 
 def parse_args() -> argparse.Namespace:
     """Parse CLI args"""
-    parser: argparse.Namespace = argparse.ArgumentParser()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
     parser.add_argument("input_filepath", help="Input file path")
-    parser.add_argument("output_filetype", help="Output file type", default="html")
-    parser.add_argument("output_filepath", help="Output file path")
-    return parser.parse_args()
+    parser.add_argument("output_filetype", choices=["html", "md"], default="html", help="Input file type (default: 'html')")
+    parser.add_argument("output_filepath", help="Output file path (optional)", nargs="?") # Optional
+    args = parser.parse_args()
+
+    if args.output_filepath is None:
+        # If filepath is `foo/bar/baz.lv`, then the line below returns `baz`
+        basename: str = os.path.splitext(os.path.basename(args.input_filepath))[0]
+        args.output_filepath = f"basename.{args.output_filetype}" # Add extension based on requested filetype
+
+    return args
 
 
 def main() -> None:
